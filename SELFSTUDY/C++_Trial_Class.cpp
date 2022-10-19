384eb72b48b3c60e603f1027a7d6303073e8a9b1
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-10-19 18:58:11 +0800

    Add files via upload

---
+++ SELFSTUDY/C++_Trial_Class.cpp
@@ -0,0 +1,20 @@
+// Defination of computer - A computer is a digital electronic machine that can be programmed to carry out sequences of arithmetic or logical operations (computation) automatically. Modern computers can perform generic sets of operations known as programs. These programs enable computers to perform a wide range of tasks. A computer system is a "complete" computer that includes the hardware, operating system (main software), and peripheral equipment needed and used for "full" operation. 
+
+//the device infront of u is called hardware in the computer world because it can be seen and touched
+
+// computers can be made to do a lot of objects such as robot programming, online games, planes, and phone, and movies.
+
+// a computer is not a computer if it only has hardware we have to program the computer if we wants it to do something. the computer language is used to program. language is a complex communication system, and its purpose is for communication. A computer language is a method of communication with a computer.
+
+// history of computer language: original pc language, binary digit '0000 0000 0000000 10000' <--> 'LOAD A, 16' it means put (load) 16 into Register A
+
+// Assemble Language 'mov ax,bx' means '1000 100111 011000' Structured program language are pascal, c, etc. Object-oriented program language: c++, java, python, etc.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
