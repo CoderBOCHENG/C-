1926afe812aa8708c17570cc67837922a9542cc8
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-10-19 20:29:50 +0800

    Create lesson1e.cpp

---
+++ SELFSTUDY/lesson1e.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"enter the current year ";
+    int bornedyears;
+    cin>>bornedyears;
+    cout<<"his age now is ->";
+    cout<<bornedyears-2000;
+}
