05a18a0625ab51f32512b40bba8bfc0e8f8fe74b
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-13 14:44:43 +0800

    Create lesson4c.cpp

---
+++ SELFSTUDY/lesson4c.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ans=0;ans2=0,ans3=0;
+    for (int n=3;n<=1000;n+=3)
+    {
+        ans+=i;
+    }
+    for (int i=5;i<=1000;i+=5)
+    {
+        ans2+=i;
+    }
+    for (int i=15;i<=1000;i+=5)
+    {
+        answer3+=i;
+    }
+
+    cout<<answer2+answer-answer3<<endl;
+
+}
