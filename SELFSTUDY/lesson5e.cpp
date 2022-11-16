ad5498dc9e7205951610806654197b46319c1498
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-16 22:18:23 +0800

    Create lesson5e.cpp

---
+++ SELFSTUDY/lesson5e.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int i;
+    int j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {        
+        for(j=1;j<=n+i-1;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    
+        return 0;
+    }
+}
