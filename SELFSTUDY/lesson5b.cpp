2aef369746aae30a792a15054515fc925735b8f3
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-16 22:17:19 +0800

    Create lesson5b.cpp

---
+++ SELFSTUDY/lesson5b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0,b=0; 
+    if (true)
+    {
+        a=a+1;
+    }
+    if (false)
+    {
+        b=b+1;
+    }
+    
+    // if (a==b)
+   
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+    return 0;
+}
