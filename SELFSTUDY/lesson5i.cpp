3c669c8b4c715c7ac4e94bd6c3099782f639873c
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-16 22:19:59 +0800

    Create lesson5i.cpp

---
+++ SELFSTUDY/lesson5i.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter a number";
+    cin>>n;
+
+    if (n%2==0)
+    {
+        cout<<"number"<<n<<"is an even number";
+    }
+
+    else
+    {
+        cout<<"number"<<n<<"is an odd number";
+    }
+    return 0;
+}
