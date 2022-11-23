79e8dd22f05f1d8ab1f978c71cd4ab586854cbb6
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-23 22:59:21 +0800

    Create lesson6e.cpp

---
+++ SELFSTUDY/lesson6e.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,fail=0;
+    cout<<"enter 2 integer (for math and eng)";
+    cin>>a>>b;
+    /*
+    if(a<60) fail++;
+    if(b<60) fail++;
+    if(fail==1) cout<<1;
+    else cout<<0;
+    */
+    if (x>=60 && y<60) || (x<60 && y>=60)
+    {
+        cout<<"1";
+    }
+    else
+    {
+        cout<<0;
+    }
+
+
+
+}
