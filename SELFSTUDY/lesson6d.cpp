66ef1e6766cee98a3883cfe2ed3f0f8011259d95
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-23 22:59:03 +0800

    Create lesson6d.cpp

---
+++ SELFSTUDY/lesson6d.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int h;
+    cout<<"enter a number";
+    cin>>h;
+    if(h>9 && h<100)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+
+
+}
