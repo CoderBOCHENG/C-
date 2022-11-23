66af7ae9ca82f46c839ee08e94898b2d088a3a20
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-11-23 22:57:46 +0800

    Create lesson6a.cpp

---
+++ SELFSTUDY/lesson6a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int h,l,w,volume,r;
+    cin>>h>>l>>w;
+    volume = h*l*w;
+    r =20000/volume;
+    if(220000%volume>0)
+        r++;
+        
+}
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+
+cout<<(1<2&&1==2);
+cout<<(1<2 || 1==2);
+cout<<(!(1<2));
+}
