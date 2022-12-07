be6badc6c0700b9cf5487ccefb7739d5fb544b3c
BC_Coder <96875741+CoderBOCHENG@users.noreply.github.com>
2022-12-07 19:14:57 +0800

    Create lesson7a.cpp

---
+++ SELFSTUDY/lesson7a.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i =1;i<100;i++){
+        cout<<i<<endl;
+        if (1>50) break;
+    
+    }
+    cout<<"this is the end";
+
+    return 0;
+}
